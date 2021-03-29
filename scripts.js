c5b78a5e442667d11f5b984d4c49eb6deb65d25c
batmit <45146744+batmit@users.noreply.github.com>
2021-03-29 17:33:29 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -123,7 +123,7 @@ const DOM = {
         <td class="description">${transaction.description}</td>
         <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
-        <td><img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="remover transação"></td>    
+        <td><img onclick="Transaction.remove(${index})" src="./minus.svg" alt="remover transação"></td>    
         
         `
         return html
